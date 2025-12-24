5f7815f7feaf93a02aa00482c92cf3fcbe3a4bda
kawu0128 <hikaruazumaguchi@gmail.com>
2025-12-24 19:01:51 +0800

    Delete src directory

--- src/container/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header } from "./header/Header";
-export { default as Date } from "./date/Date";
-export { default as Gifts } from "./gifts/Gifts";
-export { default as Github } from "./github/Github";
